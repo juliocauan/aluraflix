269012199d74338b2ff21e4984855c60e2c1f353
juliocauan <juliocauan@gmail.com>
2022-07-18 20:27:27 -0300

    Implemented UserController

--- src/main/java/br/com/juliocauan/aluraflix/controller/UserController.java
+++ src/main/java/br/com/juliocauan/aluraflix/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.juliocauan.aluraflix.infrastructure.mapper.auth.UserMapper;
 import br.com.juliocauan.aluraflix.infrastructure.model.auth.UserEntity;
+import br.com.juliocauan.aluraflix.infrastructure.model.auth.specification.UserSpecification;
 import br.com.juliocauan.aluraflix.infrastructure.repository.auth.UserRepository;
 import br.com.juliocauan.aluraflix.infrastructure.service.ProfileService;
 import br.com.juliocauan.openapi.api.UsersApi;
@@ -36,26 +37,32 @@ public class UserController implements UsersApi{
 
     @Override
     public ResponseEntity<Page<UserGet>> _findAllUsers(@Valid String search, Pageable pageable) {
-        // TODO Auto-generated method stub
-        return null;
+        Page<UserGet> response = userRepository
+            .findAll(UserSpecification.hasInEmail(search), pageable)
+            .map(userMapper::entityToGetDto);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @Override
     public ResponseEntity<UserGet> _findUserById(Long userId) {
-        // TODO Auto-generated method stub
-        return null;
+        UserEntity user = userRepository.findOneOrNotFound(userId);
+        UserGet response = userMapper.entityToGetDto(user);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @Override
     public ResponseEntity<UserGet> _updateUser(Long userId, @Valid UserPut userPut) {
-        // TODO Auto-generated method stub
-        return null;
+        UserEntity userNew = userMapper.putDtoToEntity(userPut);
+        UserEntity userOld = userRepository.findOneOrNotFound(userId);
+        userMapper.update(userNew, userOld);
+        UserGet response = userMapper.entityToGetDto(userRepository.save(userOld));
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @Override
     public ResponseEntity<Void> _deleteUser(Long userId) {
-        // TODO Auto-generated method stub
-        return null;
+        userRepository.remove(userId);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
     
 }
