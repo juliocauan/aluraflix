73953424b324f180fb24cb784446bcede810dcfc
juliocauan <juliocauan@gmail.com>
2022-07-21 22:07:52 -0300

    Inserted Admin and Client roles

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,9 @@ INSERT INTO profiles (value) VALUES ('CLIENT');
 
 INSERT INTO users (name, email, secret) VALUES
     ('Julio', 'julio@test.com', '$2a$10$ofEy..aODV5QleKty0kkJ.8UXdOXIdr/CeyXswcjJGBVYgxU296NK');
+INSERT INTO users (name, email, secret) VALUES
+    ('Cauan', 'cauan@test.com', '$2a$10$ofEy..aODV5QleKty0kkJ.8UXdOXIdr/CeyXswcjJGBVYgxU296NK');
+
+INSERT INTO users_profiles (user_entity_id, profiles_id) VALUES (1, 1);
+INSERT INTO users_profiles (user_entity_id, profiles_id) VALUES (1, 2);
+INSERT INTO users_profiles (user_entity_id, profiles_id) VALUES (2, 2);
